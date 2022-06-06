7761fd9ec38ea8e35216700102433972cb627648
sunjia72 <jialiang.sun@mail.utoronto.ca>
2022-06-05 20:58:49 -0400

    Admin User

--- phase0/src/User.java
+++ phase0/src/User.java
@@ -2,14 +2,16 @@ public class User {
     //variables
     private String userName;
     private String password;
-    private final int id;
+    private final int userid = lastid;
     private boolean admin;
+    private boolean temporarySuspended;
+
     //Constructor
-    public User(String userName, String password, boolean admin, int id){
+    public User(String userName, String password, boolean admin){
         this.userName = userName;
         this.password = password;
         this.admin = admin;
-        this.id = id;
+        this.temporarySuspended = false;
     }
     //getters and setters
     public String getUserName(){
@@ -19,7 +21,7 @@ public class User {
         return password;
     }
     public int getUserid(){
-        return id;
+        return userid;
     }
     public boolean isAdmin(){
         return admin;
@@ -29,5 +31,15 @@ public class User {
     }
     public void setUserName(String newUserName){
         userName = newUserName;
+    }
+    public void banUser(String name) {
+        if (this.admin){
+            UserManager.banUser(name);
+        }
+    }
+    public void temporarybanUser(String name, int time) {
+        if (this.admin){
+            UserManager.temporaryBanUser(name, time);
+        }
     }
 }
