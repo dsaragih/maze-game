42af0dc936c3bdc6dafc5935ea36c004ee742d5f
aaron <aa.ma@mail.utoronto.ca>
2022-06-05 20:33:22 -0400

    hi

--- phase0/src/Main.java
+++ phase0/src/Main.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
 public class Main {
+    User user;
+
+    public void run () {
+        Scanner in = new Scanner(System.in);
+        if (user == null) {
+            do {
+                String username, password;
+                //get shit
+                System.out.print("Login:\nUsername: ");
+                username = in.nextLine();
+                System.out.print("Password");
+                password = in.nextLine();
+                this.user = UserManager.login(username, password);
+            } while (user != null);
+
+        }
+        while (true) {
+            String cmd = in.nextLine();
+            if (cmd == "log") {
+                //do stuff
+            }
+            if (cmd == "") {
+                //do stuff
+            }
+
+        }
+    }
 }
+
+
