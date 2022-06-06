d46d0ad844a004c432aa5bc61ac60fb9fd773cf5
saragihd <daniel.saragih@mail.utoronto.ca>
2022-06-05 20:20:22 -0400

    added test template

--- phase0/test/LoginTests.java
+++ phase0/test/LoginTests.java
@@ -1,2 +1,15 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
 public class LoginTests {
+
+    @Test(timeout = 50)
+    public void testLogin() {
+        Momo m = new Momo(8000);
+        AlienAnimal a = new Momo(8000);
+        Tradable t = new Momo(8000);
+    }
+
+
 }
