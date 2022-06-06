e3e1935fe36789b032465e2708e30c142dc04625
P129C0A <ethan.kharitonov@gmail.com>
2022-06-05 20:35:28 -0400

    implemented userManager

--- phase0/src/UserManager.java
+++ phase0/src/UserManager.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+
 public class UserManager {
     //variables
     //constructors
     //getters and setters
     //methods
+    private int lastId = 0;
+    private ArrayList<User> users = new ArrayList<User>();
+
+    public boolean addUser(String userName, String password, boolean isAdmin){
+        for (User oldUser : users){
+            if (oldUser.getUserName() == userName){
+                return false;
+            }
+        }
+        User newUser = new User(userName, password, isAdmin);
+        return true;
+    }
+
+    public User login(String userName, String password){
+        for (User user : users){
+            if (user.getUserName() == userName && user.getPassword() == password){
+                return user;
+            }
+        }
+        return null;
+    }
 }
