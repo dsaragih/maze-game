98dea377bbe719a3706e83ff069dbfb71a88a469
kevin <kevinliustudent.liu@mail.utoronto.ca>
2022-06-05 20:05:15 -0400

    1

--- phase0/src/AdminUser.java
+++ phase0/src/AdminUser.java
@@ -1,3 +1,3 @@
-public class AdminUser {
+public class AdminUser extends User{
 
 }
--- phase0/src/User.java
+++ phase0/src/User.java
@@ -1,2 +1,9 @@
 public class User {
+    //variables
+    private String userName;
+    private String password;
+    private int userid;
+    //Constructor
+
+    //getters and setters
 }
