b5de091a1622ec0855e8711e55afc0a419d3f736
ian <ian.ewing@mail.utoronto.ca>
2022-06-16 18:43:33 -0400

    fixed logger bug

--- phase0/src/Logger.java
+++ phase0/src/Logger.java
@@ -3,12 +3,25 @@ import java.io.IOException;
 
 public class Logger{
 
+    public static FileWriter logWriter;
+
+    static {
+        try {
+            logWriter = new FileWriter("logins.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Logger() throws IOException {
+    }
+
     public static void addToLog(User user) throws IOException{
 
         try {
-            FileWriter logWriter = new FileWriter("logins.txt");
-            logWriter.write(user.getUserName() + " " + user.getPassword() + " " + user.getUserid() + "\n");
-            logWriter.close();
+            logWriter.write(user.getUserName() + " " + user.getPassword() + " " + user.getUserid());
+            logWriter.write("\r\n");
+            logWriter.flush();
         } catch (IOException e){
             System.out.println("Exception caught.");
         }
