e8a3d458fae092959c9752365acce6d58ed67483
kevin <kevinliustudent.liu@mail.utoronto.ca>
2022-07-04 19:50:49 -0400

    Created interface

--- phase1/src/g0241/Interface/Collidable.java
+++ phase1/src/g0241/Interface/Collidable.java
@@ -1,4 +1,6 @@
-package g0241.ent;
+package g0241.Interface;
+
+import g0241.ent.CollisionBox;
 
 public interface Collidable <T>{
 
--- phase1/src/g0241/Interface/Drawable.java
+++ phase1/src/g0241/Interface/Drawable.java
@@ -1,4 +1,6 @@
-package g0241.ent;
+package g0241.Interface;
+
+import g0241.ent.DrawData;
 
 public interface Drawable {
     public DrawData getDrawdata();
