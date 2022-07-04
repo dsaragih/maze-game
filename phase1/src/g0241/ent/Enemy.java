b7cd4263f6f5712898633cd9f2da640279a944ee
saragihd <daniel.saragih@mail.utoronto.ca>
2022-07-04 19:51:43 -0400

    added player movement mechanics

--- phase1/src/g0241/ent/Enemy.java
+++ phase1/src/g0241/ent/Enemy.java
@@ -1,6 +1,6 @@
 package g0241.ent;
 
-abstract public class Enemy extends Entity{
+abstract public class Enemy extends Entity {
     int hp, hpmax, xp;
     public Enemy(float x, float y) {
         super(x, y);
