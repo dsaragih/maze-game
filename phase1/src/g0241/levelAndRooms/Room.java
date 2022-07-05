881acee9a4007ae53f95c3b8677201a792cd475d
kevin <kevinliustudent.liu@mail.utoronto.ca>
2022-07-04 23:17:23 -0400

    fixed the error

--- phase1/src/g0241/levelAndRooms/Room.java
+++ phase1/src/g0241/levelAndRooms/Room.java
@@ -1,10 +1,9 @@
 package g0241.levelAndRooms;
 
 import g0241.Interface.Drawable;
-import g0241.ent.DrawData;
+import g0241.graphics.DrawData;
 
-public class Room implements Drawable {
-
+public class Room implements Drawable{
     @Override
     public DrawData getDrawdata() {
         return null;
