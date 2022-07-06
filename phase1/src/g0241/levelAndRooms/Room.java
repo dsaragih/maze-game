1a831dd16b7e7ff7e48e162d060776336d4a9fa4
kevin <kevinliustudent.liu@mail.utoronto.ca>
2022-07-06 16:01:22 -0400

    some works on Room

--- phase1/src/g0241/levelAndRooms/Room.java
+++ phase1/src/g0241/levelAndRooms/Room.java
@@ -8,18 +8,32 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Room extends DrawableEntity{
-    //The room class should implement drawable, being add to random size and texture. It should keep track of whether
+    //The room class should implement drawable, being added to random size and texture. It should keep track of whether
     //if the player is inside the room or not, while the player is inside the room, it should update the player position
+    //The room will also spawn item randomly from the available pool.
 
-    //The exact default value is indetermined yet (left open for discussion)
+    //The exact default value is undetermined yet (left open for discussion)
     private int width;
     private int height;
-    private ArrayList pos;
     private boolean playerInRoom = false;
 
     public Room(int width, int height){
         super(width,height);
     }
+    public boolean playerInRoom(){
+        return playerInRoom;
+    }
+    //to be implemented after we are done with the algorithm of generating random room
+    public void enterRoom(){
+
+    }
+    public void exitRoom(){
+
+    }
+   // public Point getPlayrrPos(){
+
+    //}
+
 
     //A filler, not implemented yet
     public void act(){
