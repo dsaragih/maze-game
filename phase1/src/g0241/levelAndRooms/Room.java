51a678bc439ac8948400880a6df4a18669b978ba
kevin <kevinliustudent.liu@mail.utoronto.ca>
2022-07-08 16:05:00 -0400

    some works on Room

--- phase1/src/g0241/levelAndRooms/Room.java
+++ phase1/src/g0241/levelAndRooms/Room.java
@@ -13,25 +13,22 @@ public class Room extends DrawableEntity{
     //if the player is inside the room or not, while the player is inside the room, it should update the player position
     //The room will also spawn item randomly from the available pool.
 
-    //The dimension of the room, the exact default value is undetermined yet (left open for discussion)
+    //The dimension of the room, the exact default value is indetermined yet (left open for discussion)
     private int width;
     private int height;
     //There is a list keeping track of event on each grid
     private ArrayList<Entity> stuff;
-    private boolean playerInRoom = false;
 
     public Room(int width, int height, ArrayList<Entity> initialEvent){
         super(width,height);
-    }
-    public boolean playerInRoom(){
-        return playerInRoom;
+        this.stuff = initialEvent;
     }
     //to be implemented after we are done with the algorithm of generating random room
-    public void enterRoom(){
-
+    public void addItem(Entity item){
+        stuff.add(item);
     }
-    public void exitRoom(){
-
+    public void removeItem(Entity item){
+        stuff.remove(item);
     }
    // public Point getPlayrrPos(){
 
