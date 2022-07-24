9061f48a2320398b05ff9440a642cc6ab0c4e760
JackSun72 <jialiang.sun@mail.utoronto.ca>
2022-07-24 13:13:13 -0400

    Add documentation

--- maze-game/core/src/com/mygdx/game/Entities/Entity.java
+++ maze-game/core/src/com/mygdx/game/Entities/Entity.java
@@ -3,23 +3,48 @@ package com.mygdx.game.Entities;
 import com.mygdx.game.geometry.Point;
 import com.mygdx.game.graphics.IDrawble;
 
+/** Represents an entity
+ * @author Ethan
+ * @author Ian Curtis Ewing
+ * @author Daniel
+ */
 public abstract class Entity implements IDrawble {
     Point pos;
 
+    /**
+     * Create an entity
+     * @param x the x-coordinate of the entity
+     * @param y the x-coordinate of the entity
+     */
     public Entity(float x, float y) {
         pos = new Point(x,y);
     }
 
+    /**
+     * Create an entity
+     * @param pos the position of the entity
+     */
     public Entity(Point pos) {
         this.pos = pos;
     }
 
+    /**
+     * Draw the entity.
+     */
     @Override
     public abstract void draw();
+
+    /**
+     * Update the entity.
+     */
     public void update() {
 
     }
 
+    /**
+     * Determine whether the entity should be removed.
+     * @return it should not be removed.
+     */
     public boolean shouldBeRemoved(){
         return false;
     }
--- maze-game/core/src/com/mygdx/game/Entities/IPlayerObserver.java
+++ maze-game/core/src/com/mygdx/game/Entities/IPlayerObserver.java
@@ -2,6 +2,16 @@ package com.mygdx.game.Entities;
 
 import com.mygdx.game.geometry.Point;
 
+/**
+ * Represents the player observer
+ * @author Ethan
+ */
 public interface IPlayerObserver {
+
+    /**
+     * Set the target
+     * @param target the target of observer.
+     */
     public void setTarget(Point target);
+
 }
