63595fb706dfd17ef48331ea4ee6545764fb31a4
JackSun72 <jialiang.sun@mail.utoronto.ca>
2022-07-24 12:56:19 -0400

    Add documentation

--- maze-game/core/src/com/mygdx/game/Entities/CollidableEnitity.java
+++ maze-game/core/src/com/mygdx/game/Entities/CollidableEnitity.java
@@ -5,17 +5,23 @@ import com.mygdx.game.geometry.Point;
 
 /** Represents a collidable object
  * @author Ethan
+ * @author Ian
  */
 public abstract class CollidableEnitity extends Entity implements ICollidable{
 
     /**
-     *
+     * Create a collidable entity with the position
      * @param pos position of the entity
      */
     public CollidableEnitity(Point pos) {
         super(pos);
     }
 
+    /**
+     * Create a collidable entity with coordinate (x,y)
+     * @param x the horizontal coordinate of the entity
+     * @param y the vertical coordinate of the entity
+     */
     public CollidableEnitity(float x, float y) {
         super(x, y);
     }
