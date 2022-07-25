da6dd3fc5c32abc01ed4aea380b56e6d0920c971
JackSun72 <jialiang.sun@mail.utoronto.ca>
2022-07-24 23:55:07 -0400

    Rename the folder

--- phase1/maze-game-wrong-one/core/src/com/mygdx/game/graphics/door/IDoorDrawer.java
+++
@@ -1,8 +0,0 @@
-package com.mygdx.game.graphics.door;
-
-import com.badlogic.gdx.graphics.Color;
-import com.mygdx.game.geometry.Point;
-
-public interface IDoorDrawer {
-    void drawDoor(Point position);
-}
--- phase1/maze-game-wrong-one/core/src/com/mygdx/game/graphics/entities/player/IPlayerDrawer.java
+++
@@ -1,8 +0,0 @@
-package com.mygdx.game.graphics.entities.player;
-
-import com.mygdx.game.geometry.Point;
-
-public interface IPlayerDrawer {
-    public void drawPlayer(Point pos, Point gunDirection);
-    public Point getGunPos(Point playerPos, Point gunDirection);
-}
