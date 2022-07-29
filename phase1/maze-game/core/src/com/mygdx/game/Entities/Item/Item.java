89ae5ce7bfe830dac41a682fca4ce61da7cc0015
kevin <kevinliustudent.liu@mail.utoronto.ca>
2022-07-29 00:40:40 -0400

    Ran into some problem with ItemFactory

--- phase1/maze-game/core/src/com/mygdx/game/Entities/Item/Item.java
+++ phase1/maze-game/core/src/com/mygdx/game/Entities/Item/Item.java
@@ -8,6 +8,7 @@ import com.mygdx.game.graphics.IDrawble;
 
 public  abstract class Item extends Entity implements IDrawble {
     //Item is an object that can be pick up by the player, and has a value. This is not a class we write in phase 1
+    //An item has a value, and can be picked up or dropped.
     private boolean onGround;
     private float value;
     public Item(float x, float y, boolean onGround, float value) {
@@ -22,6 +23,13 @@ public  abstract class Item extends Entity implements IDrawble {
 
     public float checkValue(){
         return value;
+    }
+
+    public void pickUp(){
+        onGround = false;
+    }
+    public  void drop(){
+        onGround = true;
     }
 
 }
--- phase1/maze-game/core/src/com/mygdx/game/Entities/Item/ItemFactory.java
+++ phase1/maze-game/core/src/com/mygdx/game/Entities/Item/ItemFactory.java
@@ -1,6 +1,16 @@
 package com.mygdx.game.Entities.Item;
 
-public class ItemFactory {
-    //this is not a class we write in phase 1
-//    public Item getItem(String itemName);
-}
+import com.badlogic.gdx.math.MathUtils;
+import com.mygdx.game.Entities.Gun;
+import com.mygdx.game.geometry.Point;
+
+//public class ItemFactory {
+//    //this is not a class we write in phase 1
+//    public Item getItem(String itemName){
+//        Point pos = new Point(MathUtils.random(1, 960), MathUtils.random(1, 960));
+//        Item item;
+//        if(itemName == "Gun"){
+//            item = new Gun(pos);
+//        }
+//    }
+//}
