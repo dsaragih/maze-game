12f909fdc7ddd6ef2cb9ca1f554e6160bd7f3719
JackSun72 <jialiang.sun@mail.utoronto.ca>
2022-08-06 22:51:21 -0400

    Some dependencies cannot run, so they are deleted.

--- phase1/maze-game/core/src/test/GraphicTest.java
+++ phase1/maze-game/core/src/test/GraphicTest.java
@@ -7,12 +7,12 @@ import console.launcher.Main;
 import org.junit.*;
 
 public class GraphicTest {
-    @Test(timeout=50)
-    public void ArmourTest(){
-        ExampleArmour object = new ExampleArmour(1,1);
-        Point pos = new Point(1,1);
-        CirclePlayerDrawer draw = new CirclePlayerDrawer();
-        Player player = new Player(pos,);
-    }
+//    @Test(timeout=50)
+//    public void ArmourTest(){
+//        ExampleArmour object = new ExampleArmour(1,1);
+//        Point pos = new Point(1,1);
+//        CirclePlayerDrawer draw = new CirclePlayerDrawer();
+//        Player player = new Player(pos,);
+//    }
 
 }
