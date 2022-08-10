79fde57e29b49ebe1ac25d4d28b50fb6c7052509
ivanvorobyov <ivan.vorobyov@utoronto.ca>
2022-08-10 13:26:28 -0400

    move project from phase1 folder to phase2 folder Note: if pulling, Unlink Gradle Project in tab on the right, then Link Gradle Project again.

---
+++ phase2/maze-game/core/src/game/entities/Mine.java
@@ -0,0 +1,34 @@
+package game.entities;
+
+import game.entities.abstractions.CollidableEntity;
+import game.entities.abstractions.ICollidable;
+import geometry.Circle;
+import geometry.Point;
+
+public class Mine extends CollidableEntity {
+
+    private static float mineRadius = 10;
+
+    public Mine(Point pos) {
+        super(pos);
+    }
+
+    @Override
+    public Circle getCollisionBox() {
+        return new Circle(pos, mineRadius);
+    }
+
+    @Override
+    public void informCollision(ICollidable other){
+        other.collideWith(this);
+    }
+
+    @Override
+    public void update(){
+
+    }
+
+    public static float getMineRadius(){
+        return mineRadius;
+    }
+}
